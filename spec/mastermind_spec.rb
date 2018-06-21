d10651450d4ed5540bb6784bb4d8483ac76c0caa
gratefulcheddar <timthomascode@icloud.com>
2018-06-21 12:10:41 -0500

    Refactor Mastermind#instructions test

--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -4,10 +4,8 @@ RSpec.describe "Mastermind" do
     describe "#instructions" do
         it "generates the game instructions" do
             new_game = Mastermind.new
-            expect(new_game.instructions).to eq("Welcome to Mastermind!\n" + 
-                "To play: The computer will choose a secret code comprised of 4 colors. The possible colors include RED, BLUE, GREEN, ORANGE, YELLOW, and PURPLE. The computer can choose any combination of colors, and they can be repeated.\n" + 
-                "You will then take turns guessing the 4 color sequence. If you do not guess the correct pattern within 10 attempts, you lose. After each guess round, you will be given two numbers as feedback, the number of correct colors in correct places, and the number of correct colors in incorrect places.\n" +
-                "During the guessing round, you will be able to enter input into the command line. The game will accept the correct spelling of each color, regarldess of capitalization. It will also accept a few navigational commands: !quit which will quit the game after confirmation, and !restart which starts a new game after confirmation")
+            expect(new_game.instructions).not_to be_empty
+            expect(new_game.instructions).to start_with("Welcome to Mastermind!\n")
         end
     end
 
