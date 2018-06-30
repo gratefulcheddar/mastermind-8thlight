157738cd963212fe968a43e3c644fd2874d818e7
gratefulcheddar <timthomascode@icloud.com>
2018-06-30 16:47:57 -0500

    Add update_history method to mastermind.rb with tests and refactor

--- lib/game_controller.rb
+++ lib/game_controller.rb
@@ -7,37 +7,33 @@ turn = 1
 
 while turn <= game.max_turns
 
-    puts game.color_input_message
-    
-    guess = []
-    
-    loop do
-        guess = gets.chomp.downcase.split(' ')
-        guess.map! { |color| color.to_sym }
+  puts game.color_input_message
 
-        if guess.count == game.code_length
-            break if game.validate_colors(guess)
-            puts game.invalid_color_error_message
-        else
-            puts game.wrong_number_message(guess.count)
-        end
-    
+  guess = []
+
+  loop do
+    guess = gets.chomp.downcase.split(' ')
+    guess.map! { |color| color.to_sym }
+
+    if guess.count == game.code_length
+      break if game.validate_colors(guess)
+      puts game.invalid_color_error_message
+    else
+      puts game.wrong_number_message(guess.count)
     end
-    
-    results = game.get_results(guess)
-    results[:turn] = turn
-    puts results
-    
-    if results[:black_pins] == game.code_length
-        puts game.winning_message
-        break
-    end
+  end
 
-    if turn == game.max_turns 
-        puts game.out_of_turns_message
-    end
+  result = game.get_results(guess)
+  result[:turn] = turn
+  game.update_history(result)
+  puts game.history
 
-    turn += 1
+  if result[:black_pins] == game.code_length
+    puts game.winning_message
+    break
+  end
 
+  puts game.out_of_turns_message if turn == game.max_turns
+
+  turn += 1
 end
-
