78595f6142235c35a396e959d5ee37f37d51b5f7
gratefulcheddar <timthomascode@icloud.com>
2018-06-30 16:47:57 -0500

    Add update_history method to mastermind.rb with tests and refactor

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,69 +1,73 @@
 class Mastermind
 
-    attr_reader :max_turns, :code_length, :secret_code
+  attr_reader :max_turns, :code_length, :secret_code, :history
 
-    COLOR_OPTIONS = [:red, :blue, :green, :orange, :purple, :yellow]
+  COLOR_OPTIONS = %i[red blue green orange purple yellow].freeze
 
-    def initialize(code_length = 4, max_turns = 10)
-        @code_length = code_length
-        @max_turns = max_turns
-        @secret_code = new_code
+  def initialize(code_length = 4, max_turns = 10)
+    @code_length = code_length
+    @max_turns = max_turns
+    @secret_code = new_code
+    @history = []
+  end
+
+  def instructions
+    "Welcome to Mastermind!\n\n" + 
+      "To play: The computer will choose a secret code comprised of 4 colors. The possible colors include RED, BLUE, GREEN, ORANGE, YELLOW, and PURPLE. The computer can choose any combination of colors, and they can be repeated.\n\n" + 
+      "You will then take turns guessing the 4 color sequence. If you do not guess the correct pattern within 10 attempts, you lose. After each guess round, you will be given two numbers as feedback, the number of correct colors in correct places, and the number of correct colors in incorrect places.\n\n" +
+      "During the guessing round, you will be able to enter input into the command line. The game will accept the correct spelling of each color, regardless of capitalization. It will also accept a few navigational commands: !quit which will quit the game after confirmation, and !restart which starts a new game after confirmation\n\n"
+  end
+
+  def color_input_message
+    "Guess 4 colors separated by spaces and press Enter.\n Your choices include red, blue, green, orange, purple, yellow and each color can be repeated any number of times.\n"
+  end
+
+  def invalid_color_error_message
+    "Invalid color option entered. Please guess again.\n"
+  end
+
+  def wrong_number_message(count)
+    "The secret code has 4 items, your guess had #{count}. Please guess again.\n"
+  end
+
+  def winning_message
+    "You win! Congratulations!\n"
+  end
+
+  def out_of_turns_message
+    "You have run out of turns! Sorry!\n" + "The correct code was #{secret_code}\n"
+  end
+
+  def new_code
+    Array.new(code_length) { COLOR_OPTIONS.sample }
+  end
+
+  def validate_colors(guess)
+    guess.all? { |color| Mastermind::COLOR_OPTIONS.include? color }
+  end
+
+  def get_results(guess)
+    code = @secret_code.clone
+    results = { guess: guess, black_pins: 0, white_pins: 0 }
+
+    code.each_index do |index|
+      if guess[index] == code[index]
+        results[:black_pins] += 1
+        code[index] = :not_available
+      end
     end
 
-    def instructions
-        "Welcome to Mastermind!\n\n" + 
-        "To play: The computer will choose a secret code comprised of 4 colors. The possible colors include RED, BLUE, GREEN, ORANGE, YELLOW, and PURPLE. The computer can choose any combination of colors, and they can be repeated.\n\n" + 
-        "You will then take turns guessing the 4 color sequence. If you do not guess the correct pattern within 10 attempts, you lose. After each guess round, you will be given two numbers as feedback, the number of correct colors in correct places, and the number of correct colors in incorrect places.\n\n" +
-        "During the guessing round, you will be able to enter input into the command line. The game will accept the correct spelling of each color, regardless of capitalization. It will also accept a few navigational commands: !quit which will quit the game after confirmation, and !restart which starts a new game after confirmation\n\n"
+    guess.each do |color|
+      if code.include? color 
+        results[:white_pins] += 1
+        code[code.index(color)] = :not_available
+      end
     end
 
-    def color_input_message
-        "Guess 4 colors separated by spaces and press Enter.\n Your choices include red, blue, green, orange, purple, yellow and each color can be repeated any number of times.\n"
-    end
+    results
+  end
 
-    def invalid_color_error_message
-        "Invalid color option entered. Please guess again.\n"
-    end
-
-    def wrong_number_message(count)
-        "The secret code has 4 items, your guess had #{count}. Please guess again.\n"
-    end
-
-    def winning_message
-        "You win! Congratulations!\n"
-    end
-
-    def out_of_turns_message
-        "You have run out of turns! Sorry!\n" + "The correct code was #{secret_code}\n"
-    end
-
-    def new_code
-        code_length.times.map { COLOR_OPTIONS[rand(COLOR_OPTIONS.count)] }
-    end
-
-    def validate_colors(guess)
-        guess.all? { |color| Mastermind::COLOR_OPTIONS.include? color }
-    end
-
-    def get_results(guess)
-        code = @secret_code.clone
-        results = { guess: guess, black_pins: 0, white_pins: 0 }
-
-        code.each_index do |index|
-            if guess[index] == code[index]
-                results[:black_pins] += 1
-                code[index] = :not_available
-            end
-        end
-
-        guess.each do |color|
-            if code.include? color 
-                results[:white_pins] += 1
-                code[code.index(color)] = :not_available
-            end
-        end
-
-        results
-    end
-
+  def update_history(result)
+    @history << result
+  end
 end
--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -1,94 +1,118 @@
-require "mastermind"
+require 'mastermind'
 
-RSpec.describe "Mastermind" do
+RSpec.describe 'Mastermind' do
 
-    new_game = Mastermind.new
+  let(:new_game) { Mastermind.new }
 
-    describe "#instructions" do
-        it "generates the game instructions" do
+  describe '#instructions' do
+    it 'generates the game instructions' do
 
-            expect(new_game.instructions).not_to be_empty
-            expect(new_game.instructions).to start_with("Welcome to Mastermind!\n")
-        end
+      expect(new_game.instructions).not_to be_empty
+      expect(new_game.instructions).to start_with("Welcome to Mastermind!\n")
+    end
+  end
+
+  describe '#new_code' do
+
+    let(:secret_code) { new_game.new_code }
+
+    it 'has 4 colors' do
+      expect(secret_code.count).to eq 4
     end
 
-    describe "#new_code" do
-        secret_code = new_game.new_code
-
-        it "has 4 colors" do
-            expect(secret_code.count).to eq 4
-        end
-
-        it "uses valid colors" do
-            for color in secret_code do
-                expect(Mastermind::COLOR_OPTIONS.include? color).to eq true
-            end
-        end
-
-        it "is randomly generated" do
-            is_random = false
-            (1...10).each do
-                if Mastermind.new.new_code != Mastermind.new.new_code
-                    is_random = true
-                    break
-                end
-            end
-            expect(is_random).to eq true
-        end
+    it 'uses valid colors' do
+      for color in secret_code do
+        expect(Mastermind::COLOR_OPTIONS.include? color).to eq true
+      end
     end
 
-    describe "#get_results(guess)" do
-
-        class Mastermind
-            attr_accessor :secret_code
+    it 'is randomly generated' do
+      is_random = false
+      (1...10).each do
+        if Mastermind.new.new_code != Mastermind.new.new_code
+          is_random = true
+          break
         end
+      end
+      expect(is_random).to eq true
+    end
+  end
 
-        new_game.secret_code = [:red, :blue, :green, :yellow]
+  describe '#get_results(guess)' do
 
-        it "returns 4 black pins when 4 correct colors are in the correct place" do
-
-            test_guess = new_game.secret_code
-            results = new_game.get_results(test_guess)
-            expect(results[:black_pins]).to eq 4
-            expect(results[:white_pins]).to eq 0
-
-        end
-
-        it "returns 3 black pins when 3 correct colors are in the correct place" do
-
-            test_guess = [:red, :blue, :green, :purple]
-            results = new_game.get_results(test_guess)
-            expect(results[:black_pins]).to eq 3
-            expect(results[:white_pins]).to eq 0
-        end
-
-        it "returns 0 black and white pins when there are no correct colors" do
-
-            test_guess = [:purple, :purple, :purple, :purple]
-            results = new_game.get_results(test_guess)
-            expect(results[:black_pins]).to eq 0
-            expect(results[:white_pins]).to eq 0
-        end
-
-        it "returns 2 black pins and 2 white pins when 2 correct colors are in the correct place and 2 correct colors are in the wrong place" do
-
-            test_guess = [:green, :green, :green, :yellow]
-            results = new_game.get_results(test_guess)
-            expect(results[:black_pins]).to eq 2
-            expect(results[:white_pins]).to eq 0
-        end
+    class Mastermind
+      attr_accessor :secret_code
     end
 
-    describe "#validate_colors(guess)" do
-        it "returns true if all guess colors are valid options" do
-            test_guess = [:red, :blue, :green, :yellow]
-            expect(new_game.validate_colors(test_guess)).to eq true
-        end
-
-        it "returns false if any of the guess colors are invalid options" do
-            test_guess = [:red, :blue, :oops, :yellow]
-            expect(new_game.validate_colors(test_guess)).to eq false
-        end
+    before(:each) do
+      new_game.secret_code = %i[red blue green yellow]
     end
+
+    it 'returns 4 black pins when 4 correct colors are in the correct place' do
+
+      test_guess = new_game.secret_code
+      results = new_game.get_results(test_guess)
+      expect(results[:black_pins]).to eq 4
+      expect(results[:white_pins]).to eq 0
+
+    end
+
+    it 'returns 3 black pins when 3 correct colors are in the correct place' do
+
+      test_guess = %i[red blue green purple]
+      results = new_game.get_results(test_guess)
+      expect(results[:black_pins]).to eq 3
+      expect(results[:white_pins]).to eq 0
+    end
+
+    it 'returns 0 black and white pins when there are no correct colors' do
+
+      test_guess = %i[purple purple purple purple]
+      results = new_game.get_results(test_guess)
+      expect(results[:black_pins]).to eq 0
+      expect(results[:white_pins]).to eq 0
+    end
+
+    it 'returns 2 black pins and 2 white pins when 2 correct colors are in the correct place and 2 correct colors are in the wrong place' do
+
+      test_guess = %i[green green green yellow]
+      results = new_game.get_results(test_guess)
+      expect(results[:black_pins]).to eq 2
+      expect(results[:white_pins]).to eq 0
+    end
+  end
+
+  describe '#validate_colors(guess)' do
+    it 'returns true if all guess colors are valid options' do
+      test_guess = %i[red blue green yellow]
+      expect(new_game.validate_colors(test_guess)).to eq true
+    end
+
+    it 'returns false if any of the guess colors are invalid options' do
+      test_guess = %i[red blue oops yellow]
+      expect(new_game.validate_colors(test_guess)).to eq false
+    end
+  end
+
+  describe '#update_history(result)' do
+    it 'adds turn result to the game history' do
+      test_guess = %i[red red red red]
+      result = new_game.get_results(test_guess)
+      new_game.update_history(result)
+      expect(new_game.history).to eq [result]
+    end
+
+    it 'appends to existing array in the correct turn order' do
+      test_guess_a = %i[red red red red]
+      result_a = new_game.get_results(test_guess_a)
+      new_game.update_history(result_a)
+
+      test_guess_b = %i[blue blue blue blue]
+      result_b = new_game.get_results(test_guess_b)
+      new_game.update_history(result_b)
+
+      expect(new_game.history).to eq [result_a, result_b]
+    end
+  end
 
 end
