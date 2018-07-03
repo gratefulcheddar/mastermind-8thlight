e3124339f34fc4510f58ff7981baedc991b924ca
gratefulcheddar <timthomascode@icloud.com>
2018-07-03 14:03:29 -0500

    Refactor game_controller.rb into it's own class

--- lib/game_controller.rb
+++ lib/game_controller.rb
@@ -1,39 +1,46 @@
 require_relative "../lib/mastermind"
 
-game = Mastermind.new
+class GameController
+  def initialize(args = {})
+    @game = Mastermind.new
+    @turn = 1
+    @guess = []
+  end
 
-puts game.instructions
-turn = 1
+  def play_game
+    puts @game.instructions
 
-while turn <= game.max_turns
+    while @turn <= @game.max_turns
+      puts @game.color_input_message
 
-  puts game.color_input_message
-
-  guess = []
-
-  loop do
-    guess = gets.chomp.downcase.split(' ')
-    guess.map! { |color| color.to_sym }
-
-    if guess.count == game.code_length
-      break if game.validate_colors(guess)
-      puts game.invalid_color_error_message
-    else
-      puts game.wrong_number_message(guess.count)
+      loop do
+        @guess = gets.chomp.downcase.split(' ')
+        @guess.map! { |color| color.to_sym }
+    
+        if @guess.count == @game.code_length
+          break if @game.validate_colors(@guess)
+          puts @game.invalid_color_error_message
+        else
+          puts @game.wrong_number_message(@guess.count)
+        end
+      end
+    
+      result = @game.get_results(@guess)
+      result[:turn] = @turn
+      @game.update_history(result)
+      puts @game.history
+    
+      if result[:black_pins] == @game.code_length
+        puts @game.winning_message
+        break
+      end
+    
+      puts @game.out_of_turns_message if @turn == @game.max_turns
+    
+      @turn += 1
     end
   end
-
-  result = game.get_results(guess)
-  result[:turn] = turn
-  game.update_history(result)
-  puts game.history
-
-  if result[:black_pins] == game.code_length
-    puts game.winning_message
-    break
-  end
-
-  puts game.out_of_turns_message if turn == game.max_turns
-
-  turn += 1
 end
+
+game_controller = GameController.new
+game_controller.play_game
