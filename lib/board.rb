2b8926c6ca1dffb83141250bacdac645516abf4a
gratefulcheddar <timthomascode@icloud.com>
2018-07-05 11:24:31 -0500

    Add to_s method to Board class

--- lib/board.rb
+++ lib/board.rb
@@ -8,4 +8,13 @@ class Board
   def add(guess)
     @history << guess
   end
+
+  def to_s
+    board = "\n"
+    @history.each do |turn|
+      board << "Turn \##{turn[:turn]} Black Pins = #{turn[:black_pins]}, White Pins = #{turn[:white_pins]}, Guess = #{turn[:guess]}\n"
+    end
+    board << "\n"
+    board
+  end
 end
