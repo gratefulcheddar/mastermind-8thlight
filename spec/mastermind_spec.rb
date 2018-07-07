4f97c1b713ff9d53335b87b7e4c0fc04e88f0faf
gratefulcheddar <timthomascode@icloud.com>
2018-07-06 20:49:36 -0500

    Use be_zero in tests

--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -12,46 +12,40 @@ RSpec.describe 'Mastermind' do
 
     it 'uses valid colors' do
       for color in secret_code do
-        expect(Mastermind::COLOR_OPTIONS.include? color).to eq true
+        expect(Mastermind::COLOR_OPTIONS.include?(color)).to eq true
       end
     end
   end
 
   describe '#get_result(secret_code, original_guess)' do
-
     let(:test_code) { %i[red blue green yellow] }
 
     it 'returns 4 black pins when 4 correct colors are in the correct place' do
-
       test_guess = test_code
       results = new_game.get_result(test_code, test_guess)
       expect(results[:black_pins]).to eq 4
-      expect(results[:white_pins]).to eq 0
-
+      expect(results[:white_pins]).to be_zero
     end
 
     it 'returns 3 black pins when 3 correct colors are in the correct place' do
-
       test_guess = %i[red blue green purple]
       results = new_game.get_result(test_code, test_guess)
       expect(results[:black_pins]).to eq 3
-      expect(results[:white_pins]).to eq 0
+      expect(results[:white_pins]).to be_zero
     end
 
     it 'returns 0 black and white pins when there are no correct colors' do
-
       test_guess = %i[purple purple purple purple]
       results = new_game.get_result(test_code, test_guess)
-      expect(results[:black_pins]).to eq 0
-      expect(results[:white_pins]).to eq 0
+      expect(results[:black_pins]).to be_zero
+      expect(results[:white_pins]).to be_zero
     end
 
     it 'returns 2 black pins and 2 white pins when 2 correct colors are in the correct place and 2 correct colors are in the wrong place' do
-
       test_guess = %i[green green green yellow]
       results = new_game.get_result(test_code, test_guess)
       expect(results[:black_pins]).to eq 2
-      expect(results[:white_pins]).to eq 0
+      expect(results[:white_pins]).to be_zero
     end
   end
 
