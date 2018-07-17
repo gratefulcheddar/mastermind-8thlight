11850604910237c4d0f838b44c3703f053601ea4
gratefulcheddar <timthomascode@icloud.com>
2018-07-17 17:33:36 -0500

    Separate pin calculations from results hash editing

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -29,29 +29,44 @@ class Mastermind
 
     result = { guess: original_guess, black_pins: 0, white_pins: 0 }
 
-    add_black_pins_to_result(code, guess, result)
-    add_white_pins_to_result(code, guess, result)
+    black_pins = calculate_black_pins(code, guess)
+    white_pins = calculate_white_pins(code, guess)
+
+    add_black_pins_to_result(black_pins, result)
+    add_white_pins_to_result(white_pins, result)
 
     result
   end
 
-  def add_black_pins_to_result(code, guess, result)
+  def calculate_black_pins(code, guess)
+    black_pins = 0
     code.each_index do |index|
       if guess[index] == code[index]
-        result[:black_pins] += 1
+        black_pins += 1
         code[index] = :guessed
         guess[index] = :used
       end
     end
+    black_pins
   end
 
-  def add_white_pins_to_result(code, guess, result)
+  def add_black_pins_to_result(black_pins, result)
+    result[:black_pins] = black_pins
+  end
+
+  def calculate_white_pins(code, guess)
+    white_pins = 0
     guess.each do |color|
       if code.include? color 
-        result[:white_pins] += 1
+        white_pins += 1
         code[code.index(color)] = :guessed
       end
     end
+    white_pins
+  end
+
+  def add_white_pins_to_result(white_pins, result)
+    result[:white_pins] = white_pins
   end
 
   def add_to_board(result)
