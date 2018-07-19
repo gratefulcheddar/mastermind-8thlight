f37ea2c80e555d562fc062c4f446c62e802b4030
gratefulcheddar <timthomascode@icloud.com>
2018-07-19 14:05:47 -0500

    Turn MastermindIO#get_yes_no_answer into a class method

--- lib/main.rb
+++ lib/main.rb
@@ -5,11 +5,11 @@ loop do
   game_controller.play_game
   if game_controller.guess == [:quit]
     print "Are you sure you'd like to quit? (Enter yes or no): "
-    replay = MastermindIO.new.get_yes_no_answer
+    replay = MastermindIO.get_yes_no_answer
     break if replay == :yes
   end
   print "Would you like to play again? (Enter yes or no): "
-  replay = MastermindIO.new.get_yes_no_answer
+  replay = MastermindIO.get_yes_no_answer
   break if replay == :no
   puts "\nRestarting in 3 seconds..."
   sleep(3)
