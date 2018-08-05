541cde020be8a42dd8bf0cbdb30dfdde0346c1bc
gratefulcheddar <timthomascode@icloud.com>
2018-08-05 12:56:43 -0500

    Redesign

--- spec/mastermind_io_spec.rb
+++ spec/mastermind_io_spec.rb
@@ -1,63 +1,72 @@
 require 'mastermind_io'
 
-class FakeOutputMock
-  attr_reader :message
-
-  def initialize
-    @message = ''
-  end
-
-  def puts(message)
-    @message = message
-  end
-
-  def print(message)
-    @message = message
-  end
-end
-
-class FakeInputMock
-  def gets
-    "red blue green yellow\n"
-  end
-end
-
 RSpec.describe 'MastermindIO' do
-  let(:mock_printer) { FakeOutputMock.new }
-  let(:mock_getter) { FakeInputMock.new }
-  let(:mastermind_io) { MastermindIO.new(printer: mock_printer, getter: mock_getter) }
+
+  let(:mastermind_io) { MastermindIO.new }
 
   describe '#new' do
     it 'default printer and getter do not raise errors' do
-      expect { MastermindIO.new }.to_not raise_error
+      expect { mastermind_io }.to_not raise_error
     end
   end
 
   describe '#output' do
     it "sends a message to MastermindIO's printer" do
+      expect($stdout).to receive(:puts).with('Hello, MastermindIO!')
       mastermind_io.output('Hello, MastermindIO!')
-      expect(mock_printer.message).to eq 'Hello, MastermindIO!'
     end
   end
 
   describe '#prompt' do
     it "sends a message to MastermindIO's printer" do
+      allow($stdin).to receive(:gets).and_return('something')
+      expect($stdout).to receive(:print).with('Prompt Message: ')
       mastermind_io.prompt('Prompt Message')
-      expect(mock_printer.message).to eq 'Prompt Message: '
     end
 
     it "returns a message from MastermindIO's getter" do
+      allow($stdout).to receive(:print).with('Prompt Message: ')
+      allow($stdin).to receive(:gets).and_return('red blue green yellow')
       test_input = mastermind_io.prompt('Prompt Message')
       expect(test_input).to eq 'red blue green yellow'
     end
   end
 
-  describe '#get_guess(correct_length)' do
-    context 'when guess is correct length and valid color options' do
-      it "returns a formatted guess from MastermindIO's getter" do
-        test_guess = mastermind_io.get_guess(4)
-        expect(test_guess).to eq %i[red blue green yellow]
+  describe '#get_code' do
+    it 'returns a code from stdin' do
+      allow($stdout).to receive(:print)
+      allow($stdin).to receive(:gets).and_return("red blue green yellow")
+      test_guess = mastermind_io.get_code
+      expect(test_guess).to eq %i[red blue green yellow]
+    end
+  end
+
+  describe '#get_game_mode' do
+    it 'returns a valid game mode from stdin' do
+      allow($stdout).to receive(:puts)
+      allow($stdout).to receive(:print)
+      allow($stdin).to receive(:gets).and_return("auto")
+      game_mode = mastermind_io.get_game_mode
+      expect(Rules::GAME_MODES.include?(game_mode)).to eq true
+    end
+  end
+
+  describe '#restart?' do
+    context "when user inputs 'yes'" do
+      it 'returns true' do
+        allow($stdout).to receive(:print)
+        allow($stdin).to receive(:gets).and_return("yes")
+        expect(mastermind_io.send(:restart?)).to eq true
       end
     end
+
+    context "when user inputs 'no'" do
+      it "returns false" do
+        allow($stdout).to receive(:print)
+        allow($stdin).to receive(:gets).and_return("yes")
+        expect(mastermind_io.send(:restart?)).to eq true
+      end
+    end
+    
   end
 end
